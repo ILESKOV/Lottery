8cb8ad14c30b9c66652e2e972f7875a7913a1749
Ivan Lieskov <ivan.lieskov@protonmail.com>
2022-07-18 11:28:48 +0200

    deploy scripts

---
+++ scripts/lottery.js
@@ -0,0 +1,22 @@
+const hre = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  const Lottery = await hre.ethers.getContractFactory("Lottery");
+  const lottery = await Lottery.deploy(
+    8029,
+    process.env.VRF_COORDINATOR,
+    process.env.KEY_HASH,
+    process.env.LOT_ADDRESS
+  );
+  await lottery.deployed();
+
+  console.log("Lottery deployed to:", lottery.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/token.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  let LOTcoin = await ethers.getContractFactory("LOTcoin");
+  LOT = await LOTcoin.deploy();
+
+  console.log("Token deployed to:", LOT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
