f38fd6c418f5ffb3da9966e64b376afaceffda65
Ivan Lieskov <ivan.lieskov@protonmail.com>
2022-07-18 11:22:31 +0200

    Lottery contract
    Contract use VRFv2 for generating randomness, and Data Feed price Aggregator for ETH/USD real price information
    
    Users could buy a tickets and participate in Lottery

---
+++ contracts/Token.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.15;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// TODO: Implement IERC20.sol interface and make the token mintable
+contract LOTcoin is ERC20 {
+  constructor() ERC20("Lottery Coin", "LOT") {
+    _mint(msg.sender, 5000);
+  }
+}
