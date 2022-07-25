261bdc1326d5fc54d64c34e7f49421afd67e6bbd
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-07-25 15:48:19 +0200

    all tests
    added all tests in one file

---
+++ test/_Lottery_all.js
@@ -0,0 +1,9 @@
+describe("Lottery", function () {
+    require("./startLottery")
+    require("./participate")
+    require("./endLottery")
+    require("./fulfillRandomWords")
+    require("./VRFv2")
+    require("./updateSubscriptionId")
+    require("./updateTicketPrice")
+})
