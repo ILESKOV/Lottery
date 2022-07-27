91055ed14c1f72794b837e712289a639d96c2b4e
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-07-27 02:13:22 +0200

    unit tests of updateTicketPricefunction

--- test/updateTicketPrice.js
+++ test/updateTicketPrice.js
@@ -1,39 +1,31 @@
-const { expect } = require("chai");
-const { BigNumber } = require("ethers");
-const { ethers } = require("hardhat");
-require("dotenv").config();
+const { expect } = require("chai")
+const { BigNumber } = require("ethers")
+const { ethers } = require("hardhat")
+require("dotenv").config()
 
-let owner, Lottery, VrfCoordinatorV2Mock, LOT;
+let owner, Lottery, VrfCoordinatorV2Mock, LOT
 
 beforeEach(async () => {
-  [owner, player1, player2, player3] = await ethers.getSigners();
-  let lottery = await ethers.getContractFactory("Lottery");
-  let vrfCoordinatorV2Mock = await ethers.getContractFactory(
-    "VRFCoordinatorV2Mock"
-  );
+    ;[owner, player1, player2, player3] = await ethers.getSigners()
+    let lottery = await ethers.getContractFactory("Lottery")
+    let vrfCoordinatorV2Mock = await ethers.getContractFactory("VRFCoordinatorV2Mock")
 
-  let LOTcoin = await ethers.getContractFactory("LOTcoin");
-  LOT = await LOTcoin.deploy();
+    let LOTcoin = await ethers.getContractFactory("LOTcoin")
+    LOT = await LOTcoin.deploy()
 
-  VrfCoordinatorV2Mock = await vrfCoordinatorV2Mock.deploy(0, 0);
+    VrfCoordinatorV2Mock = await vrfCoordinatorV2Mock.deploy(0, 0)
 
-  await VrfCoordinatorV2Mock.createSubscription();
-  await VrfCoordinatorV2Mock.fundSubscription(1, ethers.utils.parseEther("7"));
+    await VrfCoordinatorV2Mock.createSubscription()
+    await VrfCoordinatorV2Mock.fundSubscription(1, ethers.utils.parseEther("7"))
 
-  Lottery = await lottery.deploy(
-    1,
-    VrfCoordinatorV2Mock.address,
-    process.env.KEY_HASH,
-    LOT.address
-  );
-});
+    Lottery = await lottery.deploy(1, VrfCoordinatorV2Mock.address, process.env.KEY_HASH, LOT.address)
+})
 describe("update ticket price tests", function () {
-  it("Successfully updates ticket price", async () => {
-    expect(await Lottery.getTicketPrice()).to.equal(50);
-    await expect(Lottery.updateTicketPrice(100))
-      .to.emit(Lottery, "ParticipationFeeUpdated")
-      .withArgs(100);
-    expect(await Lottery.getTicketPrice()).to.equal(100);
-  });
-});
-
+    it("Successfully updates ticket price", async () => {
+        expect(await Lottery.getTicketPrice()).to.equal(BigNumber.from("50000000000000000000"))
+        await expect(Lottery.updateTicketPrice(10000))
+            .to.emit(Lottery, "ParticipationFeeUpdated")
+            .withArgs(BigNumber.from("10000"))
+        expect(await Lottery.getTicketPrice()).to.equal(BigNumber.from("10000"))
+    })
+})
