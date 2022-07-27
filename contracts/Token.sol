ff90aa77bb240d9f450b64499d92395873295e76
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-07-27 02:07:25 +0200

    token contract
    decimals added

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,12 +1,11 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.15;
+pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // TODO: Implement IERC20.sol interface and make the token mintable
 contract LOTcoin is ERC20 {
-  constructor() ERC20("Lottery Coin", "LOT") {
-    _mint(msg.sender, 5000);
-  }
+    constructor() ERC20("Lottery Coin", "LOT") {
+        _mint(msg.sender, 5000 * 10**18);
+    }
 }
-
