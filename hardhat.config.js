3b0e22ada0ceeb28465ad9a6a8edf86daf39f0e9
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-08-30 20:34:53 +0200

    configuration Hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,43 +1,43 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-solhint");
-require("@nomiclabs/hardhat-etherscan");
-require("solidity-coverage");
-require("dotenv").config();
-require("solidity-docgen");
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-solhint")
+require("@nomiclabs/hardhat-etherscan")
+require("solidity-coverage")
+require("dotenv").config()
+require("solidity-docgen")
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners()
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+    for (const account of accounts) {
+        console.log(account.address)
+    }
+})
 
 module.exports = {
-  solidity: {
-    version: "0.8.15",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
-      },
+    solidity: {
+        version: "0.8.15",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+            },
+        },
     },
-  },
-  networks: {
-    hardhat: {
-      initialBaseFeePerGas: 0,
+    networks: {
+        hardhat: {
+            initialBaseFeePerGas: 0,
+        },
+        rinkeby: {
+            url: process.env.RINKEBY_API,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+        ethereum: {
+            chainId: 1,
+            url: process.env.MAINNET_API,
+            accounts: [process.env.PRIVATE_KEY],
+        },
     },
-    rinkeby: {
-      url: `${process.env.RINKEBY_API}`,
-      accounts: [`${process.env.PRIVATE_KEY}`],
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_KEY,
     },
-    ethereum: {
-      chainId: 1,
-      url: `${process.env.MAINNET_API}`,
-      accounts: [`${process.env.PRIVATE_KEY}`],
-    },
-  },
-  etherscan: {
-    apiKey: `${process.env.ETHERSCAN_KEY}`,
-  },
-};
+}
