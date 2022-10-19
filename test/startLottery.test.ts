5d8635696e11ebadef2f1b5b60456f8b40ddbabc
Ivan Leskov <ivan.leskov@protonmail.com>
2022-10-19 18:58:07 +0200

    startLottery tests

--- test/startLottery.test.ts
+++ test/startLottery.test.ts
@@ -1,24 +1,54 @@
-const { expect } = require("chai")
-const { BigNumber } = require("ethers")
-const { ethers } = require("hardhat")
-require("dotenv").config()
+import { expect } from "chai"
+import { ethers } from "hardhat"
+import { ContractFactory, Contract, Signer, BigNumber } from "ethers"
 
-let owner, Lottery, VrfCoordinatorV2Mock, LOT
+const utils = ethers.utils
+const contractName = "Lottery"
+const tokenName = "LOT"
+const mockName = "VRFCoordinatorV2Mock"
+let contractFactory: ContractFactory
+let mockedContractFactory: ContractFactory
+let LOTcoin: ContractFactory
+let Lottery: Contract
+let VrfCoordinatorV2Mock: Contract
+let LOT: Contract
+let owner: Signer
+let player1: Signer
+let player2: Signer
+let player3: Signer
+let ownerWallet: string
+let player1Wallet: string
+let player2Wallet: string
+let player3Wallet: string
 
 beforeEach(async () => {
     ;[owner, player1, player2, player3] = await ethers.getSigners()
-    let lottery = await ethers.getContractFactory("Lottery")
-    let vrfCoordinatorV2Mock = await ethers.getContractFactory("VRFCoordinatorV2Mock")
 
-    let LOTcoin = await ethers.getContractFactory("LOT")
+    ownerWallet = await owner.getAddress()
+    player1Wallet = await player1.getAddress()
+    player2Wallet = await player2.getAddress()
+    player3Wallet = await player3.getAddress()
+
+    contractFactory = await ethers.getContractFactory(contractName, owner)
+    mockedContractFactory = await ethers.getContractFactory(mockName, owner)
+
+    LOTcoin = await ethers.getContractFactory(tokenName, owner)
     LOT = await LOTcoin.deploy()
 
-    VrfCoordinatorV2Mock = await vrfCoordinatorV2Mock.deploy(0, 0)
+    VrfCoordinatorV2Mock = await mockedContractFactory.deploy(0, 0)
 
     await VrfCoordinatorV2Mock.createSubscription()
-    await VrfCoordinatorV2Mock.fundSubscription(1, ethers.utils.parseEther("7"))
+    await VrfCoordinatorV2Mock.fundSubscription(1, utils.parseEther("7"))
 
-    Lottery = await lottery.deploy(1, VrfCoordinatorV2Mock.address, process.env.KEY_HASH, LOT.address, 90, 10, 50)
+    Lottery = await contractFactory.deploy(
+        1,
+        VrfCoordinatorV2Mock.address,
+        process.env.KEY_HASH,
+        LOT.address,
+        90,
+        10,
+        50
+    )
 })
 
 describe("After lottery started", function () {
@@ -30,7 +60,7 @@ describe("After lottery started", function () {
 
         it("should not allow start new lottery until state is 'CALCULATING_WINNER'", async () => {
             await Lottery.startLottery()
-            await LOT.transfer(player1.address, BigNumber.from("500000000000000000000"))
+            await LOT.transfer(player1Wallet, BigNumber.from("500000000000000000000"))
             await LOT.connect(player1).approve(Lottery.address, BigNumber.from("50000000000000000000"))
             await Lottery.connect(player1).participate()
             await Lottery.endLottery()
@@ -70,7 +100,7 @@ describe("After lottery started", function () {
 
         it("should ckeck current random word is 0 when new lottery started", async () => {
             await Lottery.startLottery()
-            await LOT.transfer(player1.address, BigNumber.from("500000000000000000000"))
+            await LOT.transfer(player1Wallet, BigNumber.from("500000000000000000000"))
             await LOT.connect(player1).approve(Lottery.address, BigNumber.from("50000000000000000000"))
             await Lottery.connect(player1).participate()
             await Lottery.endLottery()
